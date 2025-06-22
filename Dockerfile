f86388ffc186ee21ffe33e2355dc3d1105db2a91
hyeonsunny <danmi2857@naver.com>
2025-06-23 00:31:37 +0900

    project initialization

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM eclipse-temurin:17-jdk-alpine
+
+WORKDIR /app
+
+# 빌드된 JAR을 복사
+COPY build/libs/yabba-place-0.0.1-SNAPSHOT.jar app.jar
+
+# 실행
+ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/test/java/com/yabba/place/YabbaPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yabba.place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YabbaPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
